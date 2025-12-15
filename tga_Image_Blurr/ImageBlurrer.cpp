5dee9a5c5e5f954d52aa8bf83142579d04de6e90
Gustav Domeij <142096703+Quantumnuts01@users.noreply.github.com>
2025-12-15 17:28:57 +0100

    Added support for RLE Targa files

---
+++ tga_Image_Blurr/ImageBlurrer.cpp
@@ -0,0 +1,226 @@
+#include "ImageBlurrer.h"
+#include "RGBAPixel.h"
+#include <iostream>
+#include <thread>
+#include <string>
+#include<windows.h> 
+ImageBlurrer::ImageBlurrer()
+{
+}
+
+ImageBlurrer::~ImageBlurrer()
+{
+}
+
+void ImageBlurrer::BoxBlurrRGBA(std::vector<RGBAPixel>* Image, float BlurrAmmount, int StartHeightIndex, int EndHeightIndex,
+    int StartWidthIndex, int EndWidthIndex,
+    short Width, short Height)
+{
+    //std::vector<RGBAPixel> PixelSamples = {};
+    // Loop through the image and blurr it
+    for (int y = StartHeightIndex; y < EndHeightIndex; y++)
+    {
+        for (int x = StartWidthIndex; x < EndWidthIndex; x++)
+        {
+
+            RGBAPixel col = { 0, 0, 0, 0 };
+
+            int pixelsamples = 0;
+                // How many pixels in the x direction from the current pixel should be sampled
+            for (int iX = -((Width / 2) * BlurrAmmount); iX <= ((Width / 2) * BlurrAmmount); iX++)
+            {
+                // is the pixel out of bounds? if so skip that one
+                if (x + iX < 0 || x + iX >= Width)
+                    continue;
+                RGBAPixel color = { 0, 0, 0, 0 };
+                color = OriginalRGBAImage.at((y )*Width + x + iX);
+                col.R += color.R;
+                col.G += color.G;
+                col.B += color.B;
+                col.A += color.A;
+                pixelsamples++;
+            }
+
+            // Set the color sample
+            Mutex.lock();
+
+            // Set the new pixel RGBA value to the avrage of the sampled ones
+            Render1RGBAImage.at(y * Width + x).R = col.R / pixelsamples;
+            Render1RGBAImage.at(y * Width + x).G = col.G / pixelsamples;
+            Render1RGBAImage.at(y * Width + x).B = col.B / pixelsamples;
+            Render1RGBAImage.at(y * Width + x).A = col.A / pixelsamples;
+
+            // Pixelcounter to count the % of the blurring
+
+            Mutex.unlock();
+        }
+
+    }
+
+    for (int y = StartHeightIndex; y < EndHeightIndex; y++)
+    {
+        for (int x = StartWidthIndex; x < EndWidthIndex; x++)
+        {
+
+
+            RGBAPixel col = { 0, 0, 0, 0 };
+
+            int pixelsamples = 0;
+            for (int iY = -((Height/2) * BlurrAmmount); iY <= ((Height/2) * BlurrAmmount); iY++)
+            {
+                if (y + iY < 0 || y + iY >= Height)
+                    continue;
+                RGBAPixel color = { 0, 0, 0, 0 };
+                color = Render1RGBAImage.at((y + iY) * Width + x);
+                col.R += color.R;
+                col.G += color.G;
+                col.B += color.B;
+                col.A += color.A;
+                pixelsamples++;
+            }
+            // Set the color sample
+            Mutex.lock();
+
+            // Set the new pixel RGBA value to the avrage of the sampled ones
+            Image->at(y* Width + x).R = col.R / pixelsamples;
+            Image->at(y* Width + x).G = col.G / pixelsamples;
+            Image->at(y* Width + x).B = col.B / pixelsamples;
+            Image->at(y* Width + x).A = col.A / pixelsamples;
+
+            Mutex.unlock();
+        }
+    }
+}
+
+std::vector<RGBAPixel> ImageBlurrer::BlurrRGBAImage(std::vector<RGBAPixel> Image, short Width, short Height, float BlurrAmmount)
+{
+    Render1RGBAImage = Image;
+    //unsigned int coresAvilable = 4/*std::thread::hardware_concurrency()*/;
+    //OriginalRGBAImage = Image;
+    //int Xrow = 0;
+    //int Yrow = 0;
+    //std::vector<std::thread*> Threads;
+    //// more than 4? then use 4 threads
+    //for (size_t i = 0; i < coresAvilable; i++)
+    //{
+    //    //Xrow = sqrt(8);
+    //    int startHeightIndex = (Yrow * (Height / sqrt(coresAvilable)));
+    //    int EndHeightIndex = ((Height / (sqrt(coresAvilable)) * (1 + Yrow)));
+    //    int startWidthIndex = (Xrow * (Width / sqrt(coresAvilable)));
+    //    int EndWidthIndex = ((Width / sqrt(coresAvilable)) * (1 + Xrow));
+    //
+    //    std::thread* Thread1 = new std::thread(&ImageBlurrer::BoxBlurrRGBA, this, &Image, BlurrAmmount, startHeightIndex, EndHeightIndex, startWidthIndex, EndWidthIndex,
+    //        Width, Height);
+    //    Threads.push_back(Thread1);
+    //    Xrow++;
+    //    if (Xrow == sqrt(coresAvilable))
+    //    {
+    //        Xrow = 0;
+    //        Yrow++;
+    //    }
+    //}
+    //for (size_t i = 0; i < Threads.size(); i++)
+    //{
+    //    Threads[i]->join();
+    //}
+    //for (size_t i = 0; i < Threads.size(); i++)
+    //{
+    //    delete Threads[i];
+    //}
+
+    // Return the blurred image
+   // return Image;
+    // How many cores is in the system?
+        unsigned int coresAvilable = std::thread::hardware_concurrency();
+    OriginalRGBAImage = Image;
+    
+    // more than 4? then use 4 threads
+    if (coresAvilable > 4)
+    {
+
+        // math to blurr 25% of the image per thread
+        int startHeightIndex = 0;
+        int EndHeightIndex = Height - (Height / 2);
+        int startWidthIndex = 0;
+        int EndWidthIndex =  Width - (Width / 2);
+    
+        //Bottom Left
+        std::thread Thread0(&ImageBlurrer::BoxBlurrRGBA, this, &Image, BlurrAmmount, startHeightIndex, EndHeightIndex, startWidthIndex, EndWidthIndex,
+            Width, Height);
+    
+    
+        startHeightIndex = 0;
+        EndHeightIndex = Height - (Height / 2);
+        startWidthIndex = Width - (Width / 2);
+        EndWidthIndex = Width;
+    
+        //Bottom Right
+        std::thread Thread1(&ImageBlurrer::BoxBlurrRGBA, this, &Image, BlurrAmmount, startHeightIndex, EndHeightIndex, startWidthIndex, EndWidthIndex,
+            Width, Height);
+    
+        startHeightIndex = Height - (Height / 2);
+        EndHeightIndex = Height;
+        startWidthIndex = 0;
+        EndWidthIndex = Width - (Width / 2);
+    
+        //Top Left
+        std::thread Thread2(&ImageBlurrer::BoxBlurrRGBA, this, &Image, BlurrAmmount, startHeightIndex, EndHeightIndex, startWidthIndex, EndWidthIndex,
+            Width, Height);
+    
+        startHeightIndex = Height - (Height / 2);
+        EndHeightIndex = Height;
+        startWidthIndex = Width - (Width / 2);
+        EndWidthIndex = Width;
+    
+        //Top Right
+        std::thread Thread3(&ImageBlurrer::BoxBlurrRGBA, this, &Image, BlurrAmmount, startHeightIndex, EndHeightIndex, startWidthIndex, EndWidthIndex,
+            Width, Height);
+    
+        Thread0.join();
+        Thread1.join();
+        Thread2.join();
+        Thread3.join();
+
+    }
+    // more than or 2? then use 2 threads
+    else if (coresAvilable >= 2)
+    {
+        // Blurr half image per thread
+        int startHeightIndex = 0;
+        int EndHeightIndex = Height;
+        int startWidthIndex = 0;
+        int EndWidthIndex = Width / 2;
+    
+    
+        std::thread Thread0(&ImageBlurrer::BoxBlurrRGBA, this, &Image, BlurrAmmount, startHeightIndex, EndHeightIndex, startWidthIndex, EndWidthIndex,
+            Width, Height);
+    
+    
+        startHeightIndex = 0;
+        EndHeightIndex = Height;
+        startWidthIndex = Width - (Width / 2);
+        EndWidthIndex = Width;
+    
+    
+        std::thread Thread1(&ImageBlurrer::BoxBlurrRGBA, this, &Image, BlurrAmmount, startHeightIndex, EndHeightIndex, startWidthIndex, EndWidthIndex,
+            Width, Height);
+    
+    
+        Thread0.join();
+        Thread1.join();
+    }
+    // Less than 2? then use no threads very unlikley but lets call this legacy support
+    else
+    {
+        int startHeightIndex = 0;
+        int EndHeightIndex = Height;
+        int startWidthIndex = 0;
+        int EndWidthIndex = Width;
+    
+    
+        BoxBlurrRGBA(&Image, BlurrAmmount, startHeightIndex, EndHeightIndex, startWidthIndex, EndWidthIndex, Width, Height);
+    }
+    
+    // Return the blurred image
+    return Image;
+}
---
+++ tga_Image_Blurr/MainMenu.cpp
@@ -0,0 +1,63 @@
+#include "MainMenu.h"
+#include "ImageParser.h"
+#include "ImageBlurrer.h"
+#include "SImage.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+
+MainMenu::MainMenu()
+{
+    m_imageparser = new ImageParser;
+    m_imageBlurrer = new ImageBlurrer;
+    image = new SImage;
+}
+
+MainMenu::~MainMenu()
+{
+    if (image)
+        delete image;
+    if (m_imageparser)
+        delete m_imageparser;
+    if (m_imageBlurrer)
+        delete m_imageBlurrer;
+}
+
+void MainMenu::Menu()
+{
+    // Infinite for loop since i wanted to try it
+    for (;;)
+    {
+        std::cout << "Input filepath to file:\nPath with filename: ";
+
+        std::string InputString;
+        std::cin >> InputString;
+
+
+        if (!m_imageparser->ReadImage( InputString, image ))
+        {
+            std::cout << "Failed to open filepath!\n";
+            std::cout << "Please try again!\n";
+            continue;
+        }
+
+        float BlurrAmmount;
+        std::cout << "Set blurr ammount:\nblurr ammount: ";
+
+        std::cin >> BlurrAmmount;
+        if (BlurrAmmount != 0)
+        {
+            image->rgbaPixels = m_imageBlurrer->BlurrRGBAImage(image->rgbaPixels, m_imageparser->tgaHeader.width, m_imageparser->tgaHeader.height, BlurrAmmount);
+        }
+        std::cout << "Input export file name\nName:";
+        std::cin >> InputString;
+
+        InputString += ".tga";
+
+        m_imageparser->ExportImage(InputString, image);
+        break;
+    }
+}
